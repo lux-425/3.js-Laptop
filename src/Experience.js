e9022f81db2ef156bbbd74c38794c315aab809cb
luke <garcia.luc66@gmail.com>
2022-11-15 17:32:46 +0100

    iframe and text

--- src/Experience.js
+++ src/Experience.js
@@ -1,5 +1,8 @@
 import {
+  Text,
   PresentationControls,
+  ContactShadows,
+  Html,
   Float,
   Environment,
   useGLTF,
@@ -12,7 +15,7 @@ export default function Experience() {
 
   return (
     <>
-      <Environment preset='city' />
+      <Environment preset='apartment' />
 
       <color args={['#282c55']} attach='background' />
 
@@ -21,13 +24,43 @@ export default function Experience() {
         rotation={[0.13, 0.1, 0]}
         polar={[-0.4, 0.2]}
         azimuth={[-1, 0.75]}
-        config={{mass:2, tension:400}}
-        snap={{mass:4,tension:400}}
+        config={{ mass: 2, tension: 400 }}
+        snap={{ mass: 4, tension: 400 }}
       >
         <Float rotationIntensity={0.4}>
-          <primitive object={computer.scene} position-y={-1.2} />
+          <rectAreaLight
+            width={2.5}
+            height={1.65}
+            intensity={65}
+            color={'#282c55'}
+            rotation={[0.1, Math.PI, 0]}
+            position={[0, 0.55, -1.15]}
+          />
+          <primitive object={computer.scene} position-y={-1.2}>
+            <Html
+              transform
+              wrapperClass='htmlScreen'
+              distanceFactor={1.17}
+              position={[0, 1.55, -1.4]}
+              rotation-x={-0.256}
+            >
+              <iframe src='garcialuc.net' />
+            </Html>
+          </primitive>
+          <Text
+            font='./bangers-v20-latin-regular.woff'
+            fontSize={1}
+            position={[2, 0.75, 0.75]}
+            rotation-y={-1.25}
+            maxWidth={2}
+            textAlign='center'
+          >
+            LUC GARCIA
+          </Text>
         </Float>
       </PresentationControls>
+
+      <ContactShadows position-y={-1.4} opacity={0.4} scale={5} blur={2.4} />
     </>
   );
 }
