2009b62c2777c351f69005054ab82971ebed5c06
luke <garcia.luc66@gmail.com>
2022-11-15 17:09:19 +0100

    Camera and controls

--- src/Experience.js
+++ src/Experience.js
@@ -1,15 +1,33 @@
-import { OrbitControls } from '@react-three/drei'
+import {
+  PresentationControls,
+  Float,
+  Environment,
+  useGLTF,
+} from '@react-three/drei';
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+  const computer = useGLTF(
+    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf'
+  );
 
-        <OrbitControls makeDefault />
-        
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+  return (
+    <>
+      <Environment preset='city' />
 
+      <color args={['#282c55']} attach='background' />
+
+      <PresentationControls
+        global
+        rotation={[0.13, 0.1, 0]}
+        polar={[-0.4, 0.2]}
+        azimuth={[-1, 0.75]}
+        config={{mass:2, tension:400}}
+        snap={{mass:4,tension:400}}
+      >
+        <Float rotationIntensity={0.4}>
+          <primitive object={computer.scene} position-y={-1.2} />
+        </Float>
+      </PresentationControls>
     </>
+  );
 }
